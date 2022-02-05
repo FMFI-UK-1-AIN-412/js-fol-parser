ed9623ba9ca6c115ca00dc5d285bae4b3ebb508d
nikolakulikova <kulikova.n12345@gmail.com>
2022-02-05 12:52:41 +0100

    added test

--- src/__tests__/parseTff.js
+++ src/__tests__/parseTff.js
@@ -1,8 +1,13 @@
 import {chance} from 'jest-chance'
 import {chanceWS} from './helpers/chance'
-import {parseTff} from '../index'
+import {parseTff} from '../index.js'
 
-const parse = str => parseTff(str);
+import factories from './helpers/factories'
+import startRules from "../parser/startRules";
+
+const parse = (str, fs = factories) =>
+    parseTff(str, fs)
+
 
 describe('tff', () => {
   test('tff declaration', () => {
@@ -11,5 +16,12 @@ describe('tff', () => {
       role: 'formula_role',
       formula: 'formula_itself',
     });
+  });
+
+  test('vampire', () => {
+    expect(parse('tff(declare_$i1,type,jozko : $i).').toEqual(
+            ['declare_$i1', 'type', factories.constant('jozko')],
+        )
+    );
   });
 });
